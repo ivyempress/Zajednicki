28e196bca51512f008eb25181a7b8725210f83e8
Ivana <Ivana@192.168.2.11>
2014-11-21 19:07:59 +0100

    server

--- src/domen/Clan.java
+++ src/domen/Clan.java
@@ -5,10 +5,133 @@
  */
 package domen;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author Ivana
  */
-public class Clan {
+public class Clan implements Serializable{
+    
+    private int clanID;
+    private String ime;
+    private String prezime;
+    private String zanimanje;
+    private String email;
+    private Date datumRodjenja;
+    private Date datumUclanjenja;
+    private Organizacija organizacija;
+    private Grad drzava;
+    private List<Ljubimac> listaLjubimaca;
+
+    public Clan() {
+        this.drzava = new Grad();
+        this.organizacija = new Organizacija();
+        this.listaLjubimaca = new ArrayList<>();
+    }
+
+    public Clan(int clanID, String ime, String prezime, String zanimanje, String email, Date datumRodjenja, Date datumUclanjenja, Organizacija organizacija, Grad drzava, List<Ljubimac> listaLjubimaca) {
+        this.clanID = clanID;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.zanimanje = zanimanje;
+        this.email = email;
+        this.datumRodjenja = datumRodjenja;
+        this.datumUclanjenja = datumUclanjenja;
+        this.organizacija = organizacija;
+        this.drzava = drzava;
+        this.listaLjubimaca = listaLjubimaca;
+    }
+
+    public List<Ljubimac> getListaLjubimaca() {
+        return listaLjubimaca;
+    }
+
+    public void setListaLjubimaca(List<Ljubimac> listaLjubimaca) {
+        this.listaLjubimaca = listaLjubimaca;
+    }
+
+    public Organizacija getOrganizacija() {
+        return organizacija;
+    }
+
+    public void setOrganizacija(Organizacija organizacija) {
+        this.organizacija = organizacija;
+    }
+
+    public Grad getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(Grad drzava) {
+        this.drzava = drzava;
+    }
+
+    public int getClanID() {
+        return clanID;
+    }
+
+    public void setClanID(int clanID) {
+        this.clanID = clanID;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getZanimanje() {
+        return zanimanje;
+    }
+
+    public void setZanimanje(String zanimanje) {
+        this.zanimanje = zanimanje;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(Date datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public Date getDatumUclanjenja() {
+        return datumUclanjenja;
+    }
+
+    public void setDatumUclanjenja(Date datumUclanjenja) {
+        this.datumUclanjenja = datumUclanjenja;
+    }
+
+    @Override
+    public String toString() {
+        return ime+prezime;
+    }
+
+   
+         
     
 }
--- src/domen/Ljubimac.java
+++ src/domen/Ljubimac.java
@@ -5,10 +5,80 @@
  */
 package domen;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  *
  * @author Ivana
  */
-public class Ljubimac {
+public class Ljubimac implements Serializable{
+    
+    private int ljubimacID;
+    private Clan vlasnik;
+    private String imeLjubimca;
+    private Date datumSpasavanja;
+    private Zivotinja vrstaZivotinje;
+
+    public Ljubimac() {
+        this.vlasnik = new Clan();
+        this.vrstaZivotinje = new Zivotinja();
+    }
+
+    public Ljubimac(int ljubimacID, Clan vlasnik, String imeLjubimca, Date datumSpasavanja, Zivotinja vrstaZivotinje) {
+        this.ljubimacID = ljubimacID;
+        this.vlasnik = vlasnik;
+        this.imeLjubimca = imeLjubimca;
+        this.datumSpasavanja = datumSpasavanja;
+        this.vrstaZivotinje = vrstaZivotinje;
+    }
+
+    public Zivotinja getVrstaZivotinje() {
+        return vrstaZivotinje;
+    }
+
+    public void setVrstaZivotinje(Zivotinja vrstaZivotinje) {
+        this.vrstaZivotinje = vrstaZivotinje;
+    }
+
+    public int getLjubimacID() {
+        return ljubimacID;
+    }
+
+    public void setLjubimacID(int ljubimacID) {
+        this.ljubimacID = ljubimacID;
+    }
+
+    public Clan getVlasnik() {
+        return vlasnik;
+    }
+
+    public void setVlasnik(Clan vlasnik) {
+        this.vlasnik = vlasnik;
+    }
+
+    public String getImeLjubimca() {
+        return imeLjubimca;
+    }
+
+    public void setImeLjubimca(String imeLjubimca) {
+        this.imeLjubimca = imeLjubimca;
+    }
+
+    public Date getDatumSpasavanja() {
+        return datumSpasavanja;
+    }
+
+    public void setDatumSpasavanja(Date datumSpasavanja) {
+        this.datumSpasavanja = datumSpasavanja;
+    }
+
+    @Override
+    public String toString() {
+        return imeLjubimca;
+    }
+    
+    
+    
     
 }
