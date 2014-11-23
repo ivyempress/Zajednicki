405a7c80e00e1aed6d2e7d16341376d93c61eb77
Ivana <Ivana@192.168.2.6>
2014-11-23 23:31:27 +0100

    server

--- src/util/Konstante.java
+++ src/util/Konstante.java
@@ -1,23 +1,32 @@
-
-
 package util;
 
-
 public class Konstante {
-    public static final int VRATI_MESTA = 1;
-    public static final int SACUVAJ_POLAZNIKA = 2;
-    public static final int VRATI_POLAZNIKE = 3;
-    public static final int PRETRAZI_POLAZNIKE = 4;
-    public static final int OBRISI_POLAZNIKA = 5;
-    public static final int IZMENI_POLAZNIKA = 6;
-    public static final int SACUVAJ_KURS = 7;
-    public static final int VRATI_KURSEVE = 8;
-    public static final int OBRISI_KURS = 9;
-    public static final int TRAZI_KURSEVE = 10;
-    public static final int IZMENI_KURS = 11;
-    public static final int SACUVAJ_RACUN = 12;
+
+    public static final int VRATI_SVE_ORGANIZACIJE = 1;
+    public static final int VRATI_ID_ORGANIZACIJE = 2;
+    public static final int SACUVAJ_ORGANIZACIJU = 3;
+    public static final int OBRISI_ORGANIZACIJU = 4;
+    public static final int IZMENI_ORGANIZACIJU = 5;
+
+    public static final int SACUVAJ_ZIVOTINJU = 6;
+    public static final int VRATI_ID_ZIVOTINJE = 7;
+    public static final int VRATI_SVE_ZIVOTINJE = 8;
+    public static final int OBRISI_ZIVOTINJU = 9;
+    public static final int IZMENI_ZIVOTINJU = 10;
+
+    public static final int VRATI_SVE_CLANOVE = 11;
+    public static final int VRATI_SVE_LJUBIMCE = 13;
+    public static final int SACUVAJ_CLANA = 14;
+    public static final int SACUVAJ_LJUBIMCA = 15;
+    public static final int IZMENI_CLANA = 16;
+    public static final int IZMENI_LJUBIMCA = 17;
+    public static final int OBRISI_CLANA = 18;
+    public static final int OBRISI_LJUBIMCA = 19;
+    public static final int VRATI_ID_LJUBIMCA = 20;
+
+    public static final int PRETRAZI_CLANOVE = 21;
     public static final int KRAJ = 100;
-    
+
     public static final String VRATI_MESTA_OK = "Uspesno ucitana mesta!";
     public static final String VRATI_MESTA_ERROR = "Neuspesno ucitana mesta!";
     public static final String SACUVAJ_POLAZNIKA_OK = "Polaznik je uspesno sacuvan.";
@@ -27,6 +36,5 @@ public class Konstante {
     public static final String OBRISI_KURS_OK = "Kurs je uspesno obrisan.";
     public static final String IZMENI_KURS_OK = "Kurs je uspesno izmenjen.";
     public static final String SACUVAJ_RACUN_OK = "Racun je uspesno sacuvan.";
-    
-    
+
 }
